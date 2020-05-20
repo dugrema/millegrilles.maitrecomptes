a9787745fbf87f89f3dc8726dd6010fe1262d810
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-05-20 21:03:59 +0000

    Creation app reactjs pour maitredescomptes

---
+++ front-end/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>MilleGrilles</p>
+        <p>IDMG : abcd1234</p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
