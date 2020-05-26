6dd45b86e54e8dca1e2755f5d2df718008dcf026
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-05-26 15:09:14 +0000

    Ajout amqpdao dans exports

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const routeMillegrilles = require('./routes/millegrilles')
 const sessionsUsager = require('./models/sessions')
 const comptesUsagers = require('./models/comptesUsagers')
+const amqpdao = require('./models/amqpdao')
 
-module.exports = {routeMillegrilles, sessionsUsager, comptesUsagers}
+module.exports = {routeMillegrilles, sessionsUsager, comptesUsagers, amqpdao}
