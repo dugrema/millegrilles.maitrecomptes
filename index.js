f0daab5d1adfde414c97fd061a98b2f7d557b246
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-05-26 14:48:26 +0000

    Ajout module.exports

---
+++ index.js
@@ -0,0 +1,5 @@
+const routeMillegrilles = require('./routes/millegrilles')
+const sessionsUsager = require('./models/sessions')
+const comptesUsagers = require('./models/comptesUsagers')
+
+module.exports = {routeMillegrilles, sessionsUsager, comptesUsagers}
