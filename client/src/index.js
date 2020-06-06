62f487aa438f744ccfab691cd7383bb696220636
Mathieu Dugre <mathieu.dugre@mdugre.info>
2020-06-06 13:32:02 +0000

    Ajout folder containers, refact modules
    Ajout param pour websocket hot deploy

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
 // function useRedirectUrl(props) {
