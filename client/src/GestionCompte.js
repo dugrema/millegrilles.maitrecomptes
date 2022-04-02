9ffecb29935ed6ebbd3af5a845e85bb0f717d276
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-04-02 12:56:26 -0400

    Section activer delegation

--- client/src/GestionCompte.js
+++ client/src/GestionCompte.js
@@ -1,4 +1,4 @@
-import {useCallback} from 'react'
+import {useState, useCallback} from 'react'
 
 import Button from 'react-bootstrap/Button'
 import Col from 'react-bootstrap/Col'
@@ -10,15 +10,45 @@ function GestionCompte(props) {
 
     const { workers, setSectionAfficher, usagerDbLocal, confirmationCb, erreurCb } = props
 
+    const [sectionGestion, setSectionGestion] = useState('')
+
     const retourCb = useCallback( () => setSectionAfficher(''), [setSectionAfficher])
+
+    let Page
+    switch(sectionGestion) {
+        case 'SectionActiverDelegation': Page = SectionActiverDelegation; break
+        default: Page = SectionGestionComptes
+    }
 
     return (
         <>
             <h1>Gestion compte</h1>
 
-            <Button onClick={retourCb}>Retour</Button>
+            <Page 
+                workers={workers}
+                usagerDbLocal={usagerDbLocal}
+                setSectionGestion={setSectionGestion}
+                confirmationCb={confirmationCb}
+                erreurCb={erreurCb}
+                retourCb={retourCb}
+            />
+        </>
+    )
+}
 
+export default GestionCompte
+
+function SectionGestionComptes(props) {
+
+    const {workers, usagerDbLocal, setSectionGestion, confirmationCb, erreurCb, retourCb} = props
+
+    const activerDelegation = useCallback(()=>setSectionGestion('SectionActiverDelegation'), [setSectionGestion])
+
+    return (
+        <>
             <h2>Authentification</h2>
+
+            <Button onClick={retourCb}>Retour</Button>
 
             <p>Controle des methodes d'authentification pour votre compte.</p>
 
@@ -38,8 +68,35 @@ function GestionCompte(props) {
                     </BoutonAjouterWebauthn>
                 </Col>
             </Row>
-        </>
+
+            <Row>
+                <Col md={8}>
+                    Activer delegation globale (administrateur).
+                </Col>
+                <Col md={4}>
+                    <Button variant="secondary" onClick={activerDelegation}>Activer delegation</Button>
+                </Col>
+            </Row>  
+        </>      
     )
 }
 
-export default GestionCompte
+function SectionActiverDelegation(props) {
+
+    const {setSectionGestion} = props
+
+    const retourCb = useCallback(()=>setSectionGestion(''), [setSectionGestion])
+
+    return (
+        <>
+            <h2>Activer delegation</h2>
+
+            <Button onClick={retourCb}>Retour</Button>
+
+            <p>
+                Cette section permet d'utiliser la cle de millegrille pour ajouter une delegation globale
+                de type proprietaire (administrateur). 
+            </p>
+        </>
+    )
+}
