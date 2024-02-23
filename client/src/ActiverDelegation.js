f199dff629624a2f08a4cde5c9c1da45189020bb
mathieu <mathieu.dugre@mdugre.info>
2024-02-23 08:50:40 -0500

    Changement message delegation

--- client/src/ActiverDelegation.js
+++ client/src/ActiverDelegation.js
@@ -9,7 +9,7 @@ import { useTranslation } from 'react-i18next'
 import ChargerCleMillegrille, {authentiferCleMillegrille} from './ChargerCleMillegrille'
 import {getUserIdFromCertificat} from './comptesUtil'
 
-import useWorkers, {useUsagerDb} from './WorkerContext'
+import useWorkers, {useUsagerDb, useVersionCertificat} from './WorkerContext'
 import { BoutonMajCertificatWebauthn } from './WebAuthn'
 
 function SectionActiverDelegation(props) {
@@ -25,6 +25,7 @@ function SectionActiverDelegation(props) {
     const workers = useWorkers(),
           usagerDb = useUsagerDb()[0]
 
+    const setVersionCertificat = useVersionCertificat()[1]
     const [cleMillegrille, setCleMillegrille] = useState('')
     const [resultat, setResultat] = useState('')
     const [challenge, setChallenge] = useState('')
@@ -33,13 +34,19 @@ function SectionActiverDelegation(props) {
         if(!cleMillegrille || !challenge) return
         if(resultat === true) return   // Eviter generer certificat plusieurs fois
         activerDelegation(workers, usagerDb, challenge, cleMillegrille)
-            .then(()=>setResultat(true))
+            .then(reponse=>{
+                console.debug("Reponse activer delegation ", reponse)
+                setResultat(true)
+                const { delegations_date, delegations_version } = reponse
+                setVersionCertificat({ delegations_date, delegations_version })
+            })
             .then(()=>{
-                confirmationCb('Delegation completee avec succes. Le certificat de compte proprietaire est maintenant installe.')
+                // confirmationCb('Delegation completee avec succes. Le certificat de compte proprietaire est maintenant installe.')
+                confirmationCb('Delegation completee avec succes.')
                 fermer()
             })
             .catch(erreurCb)
-    }, [workers, resultat, usagerDb, setResultat, challenge, cleMillegrille, confirmationCb, erreurCb, fermer])
+    }, [workers, resultat, usagerDb, setResultat, challenge, cleMillegrille, setVersionCertificat, confirmationCb, erreurCb, fermer])
 
     // Charger un nouveau challenge de delegation
     useEffect(()=>{
